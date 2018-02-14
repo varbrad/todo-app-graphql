87a8a7863326f26dbeefe9781ea03ed43659edeb
Brad Davies <varbrad@users.noreply.github.com>
2018-02-14 21:24:51 +0000

    Initial setup
    Vagrant within www
    
    NuxtJs within app

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="container">
+    <h1>Hello!</h1>
+  </section>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  srcDir: './app',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'todo-app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'A todo-app for web dev' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  css: [
+    '@/assets/style.scss'
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
